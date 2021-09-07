6ac552da49a296594442a4cfb7ac18bf559ec7f8
Ezequiel Torres <ezequiel.torresfeyuk@gmail.com>
2021-09-07 01:22:20 -0300

    Some cleanup

--- server/main.py
+++ server/main.py
@@ -3,29 +3,7 @@
 import os
 import logging
 from configparser import ConfigParser
-
 from common.server import Server
-
-
-def parse_config_params():
-    """ Parse env variables to find program config params
-
-    Function that search and parse program configuration parameters in the
-    program environment variables. If at least one of the config parameters
-    is not found a KeyError exception is thrown. If a parameter could not
-    be parsed, a ValueError is thrown. If parsing succeeded, the function
-    returns a map with the env variables
-    """
-    config_params = {}
-    try:
-        config_params["port"] = int(os.environ["SERVER_PORT"])
-        config_params["listen_backlog"] = int(os.environ["SERVER_LISTEN_BACKLOG"])
-    except KeyError as e:
-        raise KeyError("Key was not found. Error: {} .Aborting server".format(e))
-    except ValueError as e:
-        raise ValueError("Key could not be parsed. Error: {}. Aborting server".format(e))
-
-    return config_params
 
 
 def initialize_config():
