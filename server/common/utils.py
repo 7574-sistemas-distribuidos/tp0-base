e6e3d350101dbfb5e1b5a57bfe735a17831b79cb
Cristian Raña <cristian.rana@mheducation.com>
2022-08-24 00:17:57 -0300

    New functions added

---
+++ server/common/utils.py
@@ -0,0 +1,31 @@
+import datetime
+
+
+""" Winners storage location. """
+STORAGE = "./winners"
+
+
+""" Contestant data model. """
+class Contestant:
+
+	def __init__(self, first_name, last_name, document, birthdate):
+		""" Birthdate must be passed with format: 'YYYY-MM-DD'. """
+		self.first_name = first_name
+		self.last_name = last_name
+		self.document = document
+		self.birthdate = datetime.datetime.strptime(birthdate, '%Y-%m-%d')
+
+	def __hash__(self):
+		return hash((self.first_name, self.last_name, self.document, self.birthdate))
+
+
+""" Checks whether a contestant is a winner or not. """
+def is_winner(contestant: Contestant) -> bool:
+	return hash(contestant) % 17 == 0
+
+
+""" Persist the information of each winner in the STORAGE file. Not thread-safe/process-safe. """
+def persist_winners(winners: list[Contestant]) -> None:
+	with open(STORAGE, 'a+') as file:
+		for winner in winners:
+			file.write(f'Full name: {winner.first_name} {winner.last_name} | Document: {winner.document} | Date of Birth: {winner.birthdate.strftime("%d/%m/%Y")}\n')
