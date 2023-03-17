f76db96f931cf88fb6bfcd993840d42d4e611ab6
Pablo D. Roca <pablo.roca@123seguro.com>
2023-03-17 19:23:18 -0300

    feat: add store_bets, load_bets and has_won functions

--- server/common/utils.py
+++ server/common/utils.py
@@ -2,33 +2,37 @@ import time
 import datetime
 
 
-""" Winners storage location. """
-STORAGE = "./winners"
+""" Bets storage location. """
+STORAGE = "./bets.csv"
+""" Simulated winner number in the lottery contest. """
+LOTTERY_WINNER_NUMBER = 7574
 
 
-""" Contestant data model. """
-class Contestant:
-	def __init__(self, first_name, last_name, document, birthdate):
-		""" Birthdate must be passed with format: 'YYYY-MM-DD'. """
-		self.first_name = first_name
-		self.last_name = last_name
-		self.document = document
-		self.birthdate = datetime.datetime.strptime(birthdate, '%Y-%m-%d')
-		
-	def __hash__(self):
-		return hash((self.first_name, self.last_name, self.document, self.birthdate))
-
+""" A lottery bet registry. """
+class Bet:
+    def __init__(self, first_name, last_name, document, birthdate, number):
+        """ Birthdate must be passed with format: 'YYYY-MM-DD'. """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.document = document
+        self.birthdate = datetime.datetime.strptime(birthdate, '%Y-%m-%d')
+        self.number = number
 
 """ Checks whether a contestant is a winner or not. """
-def is_winner(contestant: Contestant) -> bool:
-	# Simulate strong computation requirements using a sleep to increase function retention and force concurrency.
-	time.sleep(0.001)
-	return hash(contestant) % 17 == 0
+def has_won(bet: Bet) -> bool:
+    return bet.number == LOTTERY_WINNER_NUMBER
 
+""" Persist the information of each bet in the STORAGE file. Not thread-safe/process-safe. """
+def store_bets(bets: list[Bet]) -> None:
+    with open(STORAGE, 'a+') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_MINIMAL)
+        for bet in bets:
+            writer.writerow([bet.first_name, bet.last_name, bet.document, bet.birthdate, bet.number])
 
-""" Persist the information of each winner in the STORAGE file. Not thread-safe/process-safe. """
-def persist_winners(winners: list[Contestant]) -> None:
-	with open(STORAGE, 'a+') as file:
-		for winner in winners:
-			file.write(f'Full name: {winner.first_name} {winner.last_name} | Document: {winner.document} | Date of Birth: {winner.birthdate.strftime("%d/%m/%Y")}\n')
+""" Loads the information all the bets in the STORAGE file. Not thread-safe/process-safe. """
+def load_bets() -> list[Bet]:
+    with open(STORAGE, 'r') as file:
+        reader = csv.reader(file, quoting=csv.QUOTE_MINIMAL)
+        for row in reader:
+            yield Bet(row[0], row[1], row[2], row[3], row[4], row[5])
 
