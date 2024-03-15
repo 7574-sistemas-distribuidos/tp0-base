6e10ea318e5bf8197096365e4960ec3185863b97
Joaquin <jorivero@fi.uba.ar>
2024-03-15 16:52:39 -0300

    created script to modify docker_compose
    changes the amount of clients

---
+++ create_docker_compose.py
@@ -0,0 +1,38 @@
+import sys
+import os
+
+
+def create_client(file, number):
+    file.write('  client' + str(number) + ':\n')
+    file.write('    container_name: client' + str(number) + '\n')
+    file.write('    image: client:latest\n')
+    file.write('    entrypoint: /client\n')
+    file.write('    environment:\n')
+    file.write('      - CLI_ID=' + str(number) + '\n')
+    file.write('      - CLI_LOG_LEVEL=DEBUG\n')
+    file.write('    networks:\n')
+    file.write('      - testing_net\n')
+    file.write('    depends_on:\n')
+    file.write('      - server\n\n')
+
+
+def main():
+    argv = sys.argv
+    amount = int(argv[1])
+    with open('docker-compose-dev.yaml', 'r') as old:
+        with open('new.yaml', 'w') as new:
+            index = 0
+            lines = old.readlines()
+            while lines[index] != '  client1:\n':
+                line = lines[index]
+                new.write(line)
+                index += 1
+            
+            for i in range(1,amount+1):
+                create_client(new, i)
+                
+
+    os.remove('docker-compose-dev.yaml')
+    os.rename('new.yaml', 'docker-compose-dev.yaml')
+
+main()
