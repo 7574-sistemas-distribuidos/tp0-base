ec8df5bfc25f23ca0dc9413b39e903f8a14c19c4
palito <jaschieri@fi.uba.ar>
2024-03-22 20:02:13 -0300

    removed initial wait

--- client/common/client.go
+++ client/common/client.go
@@ -57,25 +57,12 @@ func (c *Client) createClientSocket() error {
 func (c *Client) StartClientLoop() {
 	
 	const ANSWEAR_BYTES = 1
-	// autoincremental msgID to identify every message sent
-	msgID := 1
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGTERM)
 	
 	loop:
 	// Send messages if the loopLapse threshold has not been surpassed
 	for timeout := time.After(c.config.LoopLapse); ; {
-		select {
-			case <-timeout:
-				log.Infof("action: timeout_detected | result: success | client_id: %v",
-				c.config.ID,
-				)
-				break loop
-			case <-sigs:
-				break loop
-			case <- time.After(c.config.LoopPeriod):
-		}
-
 		// Create the connection the server in every loop iteration. Send an
 		c.createClientSocket()
 
@@ -100,7 +87,6 @@ func (c *Client) StartClientLoop() {
 
 		//msg, err := bufio.NewReader(c.conn).ReadString('\n')
 		_,err = recv_exactly(c.conn, ANSWEAR_BYTES)
-		msgID++
 		c.conn.Close()
 
 		if err != nil {
@@ -114,6 +100,17 @@ func (c *Client) StartClientLoop() {
             bet.dni,
             bet.lotteryNumber,
         )
+
+		select {
+		case <-timeout:
+			log.Infof("action: timeout_detected | result: success | client_id: %v",
+			c.config.ID,
+			)
+			break loop
+		case <-sigs:
+			break loop
+		case <- time.After(c.config.LoopPeriod):
+		}
 	}
 
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
