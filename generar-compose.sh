574fa9f7e5560c66544a648f2a0d13c939c861c7
Ignacio Regazzoli <iregazzoli@fi.uba.ar>
2024-08-24 21:13:58 -0300

    Ej 2: add networks to docker-compose scripts

--- generar-compose.sh
+++ generar-compose.sh
@@ -6,16 +6,29 @@ fi
 OUTPUT_FILE=$1
 NUM_CLIENTS=$2
 
-echo "version: '3'" > $OUTPUT_FILE
-echo "services:" >> $OUTPUT_FILE
-echo "  server:" >> $OUTPUT_FILE
-echo "    image: server:latest" >> $OUTPUT_FILE
-echo "    ports:" >> $OUTPUT_FILE
-echo "      - \"8081:80\"" >> $OUTPUT_FILE
-echo "    volumes:" >> $OUTPUT_FILE
-echo "      - ./server/config.ini:/config.ini" >> $OUTPUT_FILE
-echo "" >> $OUTPUT_FILE
+echo "version: '3'"                              > $OUTPUT_FILE
+echo "services:"                                >> $OUTPUT_FILE
+echo "  server:"                                >> $OUTPUT_FILE
+echo "    image: server:latest"                 >> $OUTPUT_FILE
+echo "    ports:"                               >> $OUTPUT_FILE
+echo "      - \"8081:80\""                      >> $OUTPUT_FILE
+echo "    entrypoint: python3 /main.py"         >> $OUTPUT_FILE
+echo "    environment:"                         >> $OUTPUT_FILE
+echo "      - PYTHONUNBUFFERED=1"               >> $OUTPUT_FILE
+echo "      - LOGGING_LEVEL=DEBUG"              >> $OUTPUT_FILE
+echo "    networks:"                            >> $OUTPUT_FILE
+echo "      - testing_net"                      >> $OUTPUT_FILE
+echo "    volumes:"                             >> $OUTPUT_FILE
+echo "      - ./server/config.ini:/config.ini"  >> $OUTPUT_FILE
+echo ""                                         >> $OUTPUT_FILE
 
 python3 helpers/create_clients.py $OUTPUT_FILE $NUM_CLIENTS
+
+echo "networks:"                                >> $OUTPUT_FILE
+echo "  testing_net:"                           >> $OUTPUT_FILE
+echo "    ipam:"                                >> $OUTPUT_FILE
+echo "      driver: default"                    >> $OUTPUT_FILE
+echo "      config:"                            >> $OUTPUT_FILE
+echo "        - subnet: 172.25.125.0/24"        >> $OUTPUT_FILE
 
 echo "Succesfully modified file $OUTPUT_FILE with $NUM_CLIENTS clients."
--- helpers/create_clients.py
+++ helpers/create_clients.py
@@ -5,12 +5,16 @@ def generate_clients(filename, num_clients):
         for i in range(1, int(num_clients) + 1):
             f.write(f"  client{i}:\n")
             f.write(f"    image: client:latest\n")
+            f.write(f"    entrypoint: /client\n")
+            f.write(f"    environment:\n")
+            f.write(f"      - CLI_ID={i}\n")
+            f.write(f"      - CLI_LOG_LEVEL=DEBUG\n")
+            f.write(f"    networks:\n")
+            f.write(f"      - testing_net\n")
             f.write(f"    depends_on:\n")
             f.write(f"      - server\n")
             f.write(f"    volumes:\n")
             f.write(f"      - ./client/config.yaml:/config.yaml\n")
-            f.write(f"    environment:\n")
-            f.write(f"      - CLIENT_ID={i}\n")
 
 if __name__ == "__main__":
     output_file = sys.argv[1]
