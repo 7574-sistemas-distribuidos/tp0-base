5861a18b9022b90107838cabd7ae9bb0a6fa3470
Daniel Lovera <daanilo2497@gmail.com>
2024-08-25 14:52:00 -0300

    feature: add bash script to run scale_clients.py

---
+++ generar-compose.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+file_name=$1
+nclients=$2
+echo "Docker Compose file name: '${file_name}'"
+echo "Number of clients: '${nclients}'"
+
+poetry run -C scripts python scripts/scale_clients.py ${file_name} ${nclients}
+
+exit_code=$?
+if [ $exit_code -eq 0 ]; then
+    echo "Docker Compose file generated with ${nclients} clients"
+fi
